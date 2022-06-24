1e6dea3417e2ef3d048e7d9fc7cd0b3fb3f80c1d
FUNco <crispy.alc@gmail.com>
2022-06-24 23:01:54 +0900

    typescript-OOP-blockchain-practice

---
+++ build/index.js
@@ -0,0 +1,9 @@
+var Block = /** @class */ (function () {
+    function Block(data) {
+        this.data = data;
+    }
+    Block.hell = function () {
+        return "hi";
+    };
+    return Block;
+}());
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+class Block {
+  constructor(private data: string) {}
+  static hell() {
+    return "hi";
+  }
+}
